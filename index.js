0b559b207faecff0ed430a53f3972121eb5e522d
Andre Rahardjo <zertozzf@gmail.com>
2020-12-23 01:14:14 +0700

    feat: Finish the bot

---
+++ index.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  ws: {
+    intents: [
+      "GUILDS",
+      "GUILD_MESSAGES",
+    ],
+  },
+});
+
+let lastStickyMessage = "";
+let messageCount = 0;
+let stickyMessageChannel = "";
+let stickyMessageContent = "";
+
+client.once("ready", async function () {
+  console.info(`Bot ready! | ${client.user.username}`);
+});
+
+client.on("message", async function (message) {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content.indexOf(process.env.PREFIX) !== 0) {
+    if (stickyMessageContent !== "") {
+      if (message.channel.id === stickyMessageChannel) {
+        messageCount++;
+        if (messageCount === process.env.MAX_MESSAGE_COUNT) {
+          await lastStickyMessage.delete();
+          lastStickyMessage = await message.channel.send(stickyMessageContent);
+          messageCount = 0;
+        }
+      }
+    }
+
+    return;
+  }
+
+  const args = message.content.slice(1).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "stick") {
+    if (
+      message.author.id === process.env.OWNER
+      || message.member.roles.cache.has(process.env.ALLOWED_ROLES_ID)
+    ) {
+      try {
+        stickyMessageChannel = message.channel.id;
+        stickyMessageContent = args.slice(0).join(" ");
+        lastStickyMessage = await message.channel.send(stickyMessageContent);
+        await message.delete();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } else if (command === "unstick") {
+    if (
+      message.author.id === process.env.OWNER
+      || message.member.roles.cache.has(process.env.ALLOWED_ROLES_ID)
+    ) {
+      lastStickyMessage = "";
+      messageCount = 0;
+      stickyMessageChannel = "";
+      stickyMessageContent = "";
+      message.delete();
+    }
+  }
+});
+
+client.login();
